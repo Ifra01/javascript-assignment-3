73290f1449b6e913fe4f0859ba167431ebc141dc
Ifra01 <jamalifra74@gmail.com>
2025-02-09 21:08:46 +0500

    created app.js

---
+++ app.js
@@ -0,0 +1,46 @@
+// ASSIGNMENT 3
+// Chapter 9-11 - USER INPUT & CONDITIONAL STATEMENT
+
+// Question 1 Write a program to take “city” name as input from user. If
+// user enters “Karachi”, welcome the user like this:
+// “Welcome to city of lights”
+// answer:
+// let cityName = prompt("enter your city name");
+// let city = "karachi";
+// if (city == cityName){
+//     alert("welcome to the city of lights")
+// }
+// else{
+//     alert("You doesn't belong to city of lights")
+// }
+
+//Question 2 Write a program to take “gender” as input from user. If the
+// user is male, give the message: Good Morning Sir. If the
+// user is female, give the message: Good Morning Ma’am.
+// answer: 
+// let userGender = prompt("enter your gender");
+// let gender = "male";
+// if(gender == userGender){
+//     alert("Good morning sir");
+// }
+// else{
+//     alert("Good morning ma'am")
+// }
+
+// Question 3 Write a program to take input color of road traffic signal
+// from the user & show the message according to this table
+// answer:
+// let trafficSignal = prompt("enter any signal color");
+// let color1 = "red";
+// let color2 = "green";
+// if (trafficSignal != color2) {
+//     if (color1 == trafficSignal) {
+//         alert("must stop");
+//     }
+//     else {
+//         alert("ready to move");
+//     }
+// }
+// else {
+//     alert("Move now")
+// }
